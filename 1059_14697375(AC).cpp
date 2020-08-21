20cfedc80491949574061f2e9297574a56ee4060
wonchan-lee <wonchan-lee@github.com>
2020-08-21 11:27:41 +0900

    2020.08.21

---
+++ 1059_14697375(AC).cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x; 
+    cout << ~x; // x의 2진수 형태의 1's 보수형을 출력
+}
---
+++ 1060_14697465(AC).cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >>y;
+    // 2진수의 각 자릿수의 and연산
+    int z = x&y; 
+    // 따로 이렇게 담아서 출력해야댐
+    cout << z; 
+}
