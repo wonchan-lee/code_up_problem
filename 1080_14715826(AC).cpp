a20330effad4853b2f550b26379a98ff4ace0a35
wonchan-lee <wonchan-lee@github.com>
2020-08-23 09:53:48 +0900

    2020.08.23

---
+++ 1080_14715826(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int limit;
+    int i=0;
+    int sum=0;
+    cin >> limit;
+    
+    while(sum < limit){
+        i++;
+        sum+=i;
+    }
+    cout << i;
+}
---
+++ 1082_14715847(AC).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> hex >> x;
+    
+    for (int i=1; i<=15 ; i++){
+        cout << uppercase <<hex <<x <<"*"<<i<<"=";
+        cout << uppercase <<hex <<x*i<<'\n';
+    }
+}
