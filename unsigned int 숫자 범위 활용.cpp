85ded69d3663e8b783a6021e38fc4270501a8462
wonchan-lee <wonchan-lee@github.com>
2020-08-25 01:48:36 +0900

    2020.08.24

---
+++ unsigned int 숫자 범위 활용.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int h, b, c, s;
+    double tmp, result;
+    cin >> h >> b >> c >> s;
+    unsigned int tmp1= h*b*c*s;
+    tmp = 8*1024*1024;
+    
+    result = tmp1/tmp;
+    printf("%.1lf MB",result);
+    }
